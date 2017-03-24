dd67eeedf4a53074aa59d325a3bfd4d57d99956c
davejoshmike <davejoshmike@gmail.com>
2017-03-24 15:58:26 -0600

    Made UI changes in popup. List button doesn't work so it has been commented out

--- popup.js
+++ popup.js
@@ -4,26 +4,34 @@
  */
 
 var bg = chrome.extension.getBackgroundPage();
+var passAudit = bg.passAudit;
+show_pass_fail(passAudit);
+// show_list(passAudit);
 
-var passes = bg.passAudit;
-
-if (passes != null) {
-	if (passes) {
-		$("#passFailLabel").text("Audit Passed");
-		$("#popupIcon").prepend("<img align='center' src='icon/icon128x128.png'>");
+function show_pass_fail(passes) {
+	if (passes != null) {
+		if (passes) {
+			$("#passFailLabel").text("Audit Passed");
+			$("#popupIcon").prepend("<img align='center' src='icon/icon128x128.png'>");
+		} else {
+			$("#passFailLabel").text("Audit Failed");
+			$("#popupIcon").prepend("<img align='center' src='icon/fail-icon128x128.png'>");
+		}
 	} else {
-		$("#passFailLabel").text("Audit Failed");
-		$("#popupIcon").prepend("<img align='center' src='icon/fail-icon128x128.png'>");
+		$("#passFailLabel").text("Unknown Audit Result");
+		$("#popupIcon").prepend("<img align='center' src='icon/icon128x128.png'>");
 	}
-} else {
-	$("#passFailLabel").text("Unknown Audit Result");
-	$("#popupIcon").prepend("<img align='center' src='icon/icon128x128.png'>");
 }
+
+// function show_list(passes){
+// 	if (!passes) {
+// 		$("#buttonDiv").append("<button href='#' id='list'>List</button>");
+// 	}
+// }
 
 function run_audit(){
 	bg.getAndCheckConfig(suppressAlert=false);
 }
-
 
 /**
  * Close the window
@@ -32,6 +40,18 @@ function close_window(){
   window.close();
 }
 
+// function list_BadAddons(){
+// 	console.log("BadAddons: \n");
+// 	bg.get_badAddons(function(badAddons){
+// 		console.log(badAddons);
+// 		alert("Bad Addons: \n" + badAddons.join("\n"));
+// 	});
+// }
+
 // add listeners to rerun button
 document.getElementById('rerun').addEventListener('click', run_audit);
 document.getElementById('rerun').addEventListener('click', close_window);
+
+
+// document.getElementById('list').addEventListener('click', list_BadAddons);
+// document.getElementById('list').addEventListener('click', close_window);
