0c5d083a34000b65681140f35c07e55644cd1c0b
Ivan <ioa13a@acu.edu>
2017-06-28 15:58:07 -0500

    Uncommented some codes to see what they do

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@
 var bg = chrome.extension.getBackgroundPage();
 var passAudit = bg.passAudit;
 show_pass_fail(passAudit);
-// show_list(passAudit);
+show_list(passAudit);
 
 function show_pass_fail(passes) {
 	if (passes != null) {
@@ -23,11 +23,11 @@ function show_pass_fail(passes) {
 	}
 }
 
-// function show_list(passes){
-// 	if (!passes) {
-// 		$("#buttonDiv").append("<button href='#' id='list'>List</button>");
-// 	}
-// }
+ function show_list(passes){
+ 	if (!passes) {
+ 		$("#buttonDiv").append("<button href='#' id='list'>List</button>");
+ 	}
+ }
 
 /*
 * Triggered by clicking the "Run Audit" button in the popup, this runs an audit, 
@@ -44,18 +44,18 @@ function close_window(){
   window.close();
 }
 
-// function list_BadAddons(){
-// 	console.log("BadAddons: \n");
-// 	bg.get_badAddons(function(badAddons){
-// 		console.log(badAddons);
-// 		alert("Bad Addons: \n" + badAddons.join("\n"));
-// 	});
-// }
+ function list_BadAddons(){
+ 	console.log("BadAddons: \n");
+ 	bg.get_badAddons(function(badAddons){
+ 		console.log(badAddons);
+ 		alert("Bad Addons: \n" + badAddons.join("\n"));
+ 	});
+ }
 
 // add listeners to rerun button
 document.getElementById('rerun').addEventListener('click', run_audit);
 document.getElementById('rerun').addEventListener('click', close_window);
 
 
-// document.getElementById('list').addEventListener('click', list_BadAddons);
-// document.getElementById('list').addEventListener('click', close_window);
+document.getElementById('list').addEventListener('click', list_BadAddons);
+document.getElementById('list').addEventListener('click', close_window);
